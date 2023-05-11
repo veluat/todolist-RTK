4a7707ee80c29a6920aa66fbe012638c3d9eb026
Julia <veluat@gmail.com>
2023-05-12 00:31:02 +0300

    Storybook was updated to version 7.0.10

---
+++ src/Components/ErrorSnackBar/ErrorSnackBar.stories.tsx
@@ -0,0 +1,23 @@
+import {Meta, StoryFn, StoryObj} from "@storybook/react";
+import {ErrorSnackBar} from "./ErrorSnackBar";
+import {Alert, Snackbar} from "@mui/material";
+import React from "react";
+
+const meta: Meta<typeof ErrorSnackBar> = {
+    component: ErrorSnackBar,
+    title: 'TODOLISTS/ErrorSnackBar'
+};
+export default meta;
+
+type Story = StoryObj<typeof ErrorSnackBar>;
+
+export const ErrorSnackBarStory: StoryFn = (args) => {
+    const error = 'Some going wrong. Try later.';
+    return (
+        <Snackbar open={true}>
+            <Alert severity="error" sx={{width: '100%'}}>
+                {error}
+            </Alert>
+        </Snackbar>
+    )
+}
