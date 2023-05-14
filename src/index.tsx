72db5a33aa8fbfab05885c49afc927b9160ecc1e
Julia <veluat@gmail.com>
2023-05-14 23:42:07 +0300

    some import fixed

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import CssBaseline from '@mui/material/CssBaseline';
-import {App} from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 import {createRoot} from 'react-dom/client';
 import {createTheme, ThemeProvider} from "@mui/material/styles";
+import {App} from "./app/App";
 
 const theme = createTheme({
     palette: {
